729813898a5dbe15183d201ad7b55b0fb7f5f51e
Christina <Christinepeninsula@hotmail.com>
2019-01-29 09:04:44 -0500

    update naming of functions

--- main.js
+++ main.js
@@ -15,19 +15,20 @@
         // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
-    function max_of_three(a,b,c) {
-      if(a >= b){
-        if(a > c){
-        return a;
-
-      }if {
+    function maxOfThree(a,b,c) {
+      if (a >= b) {
+        if (a > c) {
+          return a;
+        } else {
           return c;
-        }elseif (b >= c){
+
+        }elseif(b >= c){
         return b;
 
       }else{
         return c;
       }
+    }
 
 
 
@@ -37,29 +38,29 @@
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
 
-  function is_vowel(x):
-    var vowels = ["a", "e", "i", "o", "u", "y," "w"];
+  function isVowel(x) {
+    var vowels = ["a", "e", "i", "o", "u", "y,", "w"];
     if (x in vowels) {
-      return True
+      return True;
 
-    }else {
+    } else {
       return False;
     }
 
-
+  }
 
 
 
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
-    function translate(phrase) {
+    function rovarspraket(phrase) {
       var rov = [];
       for (i in phrase) {
         rov.append(str(i) + "o" + str(i));
       }
       var last = "".join(rov);
-      return last
+      return last;
     }
 
   
@@ -67,14 +68,15 @@
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
   // ---------------------
-    function multiply(n){
-    var total = 1;
-    for(i in range(0,len(n))) {
-      var total;
-      *= n[i];
-
-      print(total)
-    }
+  //   function multiply(n){
+  //   var total = 1;
+  //   for(i in range(0,len(n))) {
+  //     var total
+  //     *= n[i];
+  //   }
+  //
+  //     print(total)
+  //   }
 
 
 
@@ -85,7 +87,7 @@
   // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
   // ---------------------
     function reverse(a) {
-      var last = ''.join(list(reversed(a)))
+      var last = ''.join(list(reversed(a)));
       return last
     }
 
@@ -94,25 +96,25 @@
   // ---------------------
   // Define a function findLongestWord() that takes an array of words and returns the length of the longest one.
   // ---------------------
-    function find_longest_word(list){
+    function findLongestWord(list){
       var longest_word = " ";
-      for (i in list){
-.        if(len(i) > len(longest_word){
+      for(i in list){
+.        if (length(i) > length(longest_word){
             longest_word = i}
       }
-      return len(longest_word)
+      return length(longest_word)
     }
-  
+//
 
   // ---------------------
   // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
   // ---------------------
 
-    function filter_long_words(words, a) {
+    function filterLongWords(words, a) {
       var longest_word = "";
       var longest_word_greater = [];
       for (i in words) {
-        if (len(i) > len(longest_word)){
+        if (length(i) > length(longest_word)){
           longest_word = i;
         }else{
 
